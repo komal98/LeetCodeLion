708fac58be77b994b7439bbdd2395de7c155b74e
Komal Bakshi <kbakshi98@gmail.com>
2023-06-18 11:27:14 -0700

    Time: 1 ms (100%), Space: 41.4 MB (61.93%) - LeetHub

--- 1904-second-largest-digit-in-a-string/1904-second-largest-digit-in-a-string.java
+++ 1904-second-largest-digit-in-a-string/1904-second-largest-digit-in-a-string.java
@@ -1,16 +1,26 @@
 class Solution {
     public int secondHighest(String s) {
-       TreeSet<Integer> set = new TreeSet<>();
+        int max = -1;
+        int secondMax = -1;
 
-        for(int i = 0; i < s.length(); i++){
-            if(Character.isDigit(s.charAt(i))){
-                set.add(Character.getNumericValue(s.charAt(i)));  
+        char[] characters = s.toCharArray();
+
+        for(int i = 0; i < characters.length; i++){
+            if(characters[i]>='0' && characters[i]<='9'){
+                int val = characters[i]-'0';
+                if(val>max){
+                    secondMax = max;
+                    max = val;
+                }
+                else if(val>secondMax && val!=max){
+                    secondMax = val;
+                }
             }
         }
-        if(set.size()== 0 || set.size() == 1){
+        if(secondMax == max){
             return -1;
         }
-        set.pollLast();
-        return set.last();
+        return secondMax;
+        
     }
 }
