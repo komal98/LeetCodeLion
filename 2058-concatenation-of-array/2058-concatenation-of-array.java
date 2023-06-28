3bf28c7295bc792550d51d04d6d391b7ca9b5f84
Komal Bakshi <kbakshi98@gmail.com>
2023-06-28 16:45:31 -0700

    Time: 1 ms (93.4%), Space: 44.6 MB (20.37%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.java
+++ 2058-concatenation-of-array/2058-concatenation-of-array.java
@@ -3,8 +3,7 @@ class Solution {
         int n = nums.length;
         int[] ans = new int[2*n];
         for(int i = 0; i<n; i++){
-            ans[i] = nums[i];
-            ans[i+n] = nums[i];
+            ans[i] = ans[i+n] = nums[i];
         }
         return ans;
     }
