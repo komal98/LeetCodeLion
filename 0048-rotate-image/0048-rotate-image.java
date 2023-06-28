215006658caec9c377667c114af322b0d9121981
Komal Bakshi <kbakshi98@gmail.com>
2023-06-28 16:30:35 -0700

    Time: 0 ms (100%), Space: 41.1 MB (91.57%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -13,15 +13,21 @@ class Solution {
 
         //reverse each row of matrix
         for(int i = 0; i<n; i++){
-            int low = 0, high = n-1;
-            while(low<=high){
-                int temp = matrix[i][high];
-                matrix[i][high] = matrix[i][low];
-                matrix[i][low] = temp;
-                high--;
-                low++; 
+            int start = 0, end = n-1;
+            while(start<=end){
+                int temp = matrix[i][end];
+                matrix[i][end] = matrix[i][start];
+                matrix[i][start] = temp;
+                end--;
+                start++; 
             }
         }
         
     }
 }
+/*
+[1,2,3 transpose   [1,4,7  reverse row    [7,4,1
+ 4,5,6 -----------> 2,5,8 -------------->  8,5,2
+ 7,8,9              3,6,9]                 9,6,3]
+ ]
+*/
