a9474ed1056844f87245e5e0b0263f16fc1ef2dc
Komal Bakshi <kbakshi98@gmail.com>
2023-07-01 16:27:49 -0700

    Time: 1 ms (99.98%), Space: 44.5 MB (35.52%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,22 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
-     
-        int reach = 0;
-        
-        //at every step calculate the reachability and keep updating it till we reach end 
-        
-        for(int i = 0; i < nums.length; ++i){
-            
-            if(reach < i)
-                return false;
-            
-            reach = Math.max(reach, i + nums[i]);
-            
-            if(reach == nums.length)
-                return true;
-        }
-        
-        return true;
-        
+        int goal = nums.length-1;
+       for(int i = goal-1; i >= 0; i--){
+           if(nums[i]+i >= goal){
+               goal = i;
+           }
+       }
+       return goal == 0;
     }
 }
