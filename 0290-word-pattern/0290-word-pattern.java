c86deccc3c0c2b08eb0d2b9ef47ec6a64ba8404e
Komal Bakshi <kbakshi98@gmail.com>
2023-07-07 17:19:33 -0700

    Time: 1 ms (61.53%), Space: 40.4 MB (87.32%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,25 +1,23 @@
 class Solution {
-    public boolean wordPattern(String pattern, String s) {
-       HashMap<Character, String> sMap = new HashMap<>();
-       HashMap<String, Character> pMap = new HashMap<>();
-
-       String[] words = s.split(" ");
-       if(words.length != pattern.length()) return false;
-       for(int i = 0; i<pattern.length(); i++){
-           char ch = pattern.charAt(i);
-
-           if(sMap.containsKey(ch)){
-               if(!sMap.get(ch).equals(words[i]))
-                    return false;
-           }
-           sMap.put(ch, words[i]);
-
-           if(pMap.containsKey(words[i])){
-               if(!pMap.get(words[i]).equals(ch))
-                    return false;
-           }
-           pMap.put(words[i],ch);
-       }
+   public boolean wordPattern(String pattern, String s) {
+        List<String> list = List.of(s.split(" "));
+        Map<Character, String> map = new HashMap<>();
+        if (pattern.length() != list.size()) {
+            return false;
+        }
+        return (extracted(pattern, list, map));
+    }
+    private static boolean extracted(String pattern, List<String> list, Map<Character, String> map) {
+        for (int i = 0; i < pattern.length(); i++) {
+             if (!map.containsKey(pattern.charAt(i))) {
+                if(map.containsValue(list.get(i))) return false;
+                map.put(pattern.charAt(i), list.get(i));
+            } else if (map.containsKey(pattern.charAt(i))
+                    && !map.get(pattern.charAt(i)).equals(list.get(i))) {
+               return false;
+            }
+        }
         return true;
     }
+
 }
