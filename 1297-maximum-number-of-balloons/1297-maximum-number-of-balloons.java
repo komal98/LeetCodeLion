adb5793f69200f1ca0063d5d0685dfa0f540eb80
Komal Bakshi <kbakshi98@gmail.com>
2023-07-11 14:54:51 -0700

    Time: 2 ms (88.35%), Space: 41.1 MB (76.56%) - LeetHub

---
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        int b = 0, a = 0, l = 0, o = 0, n = 0;
+        for( char c : text.toCharArray()){
+            switch(c){
+                case 'b' : b++;
+                break;
+                
+                case 'a' : a++;
+                break;
+
+                case 'l' : l++;
+                break;
+
+                case 'o' : o++;
+                break;
+
+                case 'n' : n++;
+                break;
+            }
+        }
+       return Math.min(Math.min(Math.min(b,a),n), Math.min(l/2,o/2)); 
+    }
+}
+/*
+b - 1
+a - 1
+l - 2
+o - 2
+n - 1
+*/
