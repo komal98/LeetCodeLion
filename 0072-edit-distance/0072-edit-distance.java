da13d8b16f4c14a34f7bf9a385c6ce66a689c730
Komal Bakshi <kbakshi98@gmail.com>
2023-09-22 17:50:09 -0700

    Time: 5 ms (68.02%), Space: 43.8 MB (33.27%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+       int m = word1.length();
+       int n = word2.length();
+
+       int[][]dp = new int[m+1][n+1];
+
+       /* when words are of different length for those extra chracters  default we will fill Int max */
+
+       for(int i = 0; i < m; i++){
+           Arrays.fill(dp[i], Integer.MAX_VALUE);
+       }
+
+       /*Since this is recursive bottom up appraoch we need to fill out the base case. Which is '-' and character match row wise and column wise */
+       for(int i = 0; i <=m; i++){
+           dp[i][0] = i;
+       }
+       
+       for(int j = 0; j <=n; j++){
+           dp[0][j]= j;
+       }
+
+        for(int i = 1; i <=m; i++){
+            for(int j = 1; j <=n; j++){
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    int min = Math.min(dp[i][j-1], dp[i-1][j]);
+                    dp[i][j] = 1+Math.min(dp[i-1][j-1],min);
+                }
+            }
+        }
+    return dp[m][n];
+    }
+}
