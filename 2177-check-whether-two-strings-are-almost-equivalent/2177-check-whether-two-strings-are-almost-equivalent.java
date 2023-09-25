811771bf5cdfe2f97e2c726f450d5f04a8acf274
Komal Bakshi <kbakshi98@gmail.com>
2023-09-25 11:39:19 -0700

    Time: 1 ms (83.06%), Space: 40.2 MB (97.59%) - LeetHub

--- 2177-check-whether-two-strings-are-almost-equivalent/2177-check-whether-two-strings-are-almost-equivalent.java
+++ 2177-check-whether-two-strings-are-almost-equivalent/2177-check-whether-two-strings-are-almost-equivalent.java
@@ -1,18 +1,12 @@
 class Solution {
     public boolean checkAlmostEquivalent(String word1, String word2) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        int n = word1.length();
-
-        for(int i = 0; i<n; i++){
-            map.put(word1.charAt(i), map.getOrDefault(word1.charAt(i),0)+1);
-            map.put(word2.charAt(i), map.getOrDefault(word2.charAt(i),0)-1);
+        int []arr=new int [26];
+        for (int i=0;i<word1.length();i++)
+        {
+            arr[word1.charAt(i)-'a']++;
+            arr[word2.charAt(i)-'a']--;
         }
-
-        for(int i : map.values()){
-            if(i > 3 || i < -3){
-                return false;
-            }
-        }
+        for (int i:arr){if (Math.abs(i)>3){return false;}}
         return true;
     }
 }
