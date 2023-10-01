8f875583c1e4a773c15a0d7978908680a1a15eb3
Komal Bakshi <kbakshi98@gmail.com>
2023-10-01 12:31:34 -0700

    Time: 9 ms (21.07%), Space: 43.7 MB (26.63%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,54 +1,19 @@
 class Solution {
-
-    class Trie{
-        boolean isword;
-        HashMap<Character, Trie> child;
-
-        Trie(){
-            isword = false;
-            child = new HashMap<>();
-        }
-    }
-
-    Trie root = new Trie();
-
     public boolean wordBreak(String s, List<String> wordDict) {
-        root = new Trie();
-
-        for(String it : wordDict){
-            Trie node = root;
-
-            for(char ele : it.toCharArray()){
-                if(!node.child.containsKey(ele)){
-                    node.child.put(ele, new Trie());
-                }
-
-                node = node.child.get(ele);
-            }
-
-            node.isword = true;
-        }
-        boolean[] dp = new boolean[s.length()];
-
-        for(int i = 0;i < s.length();i++){
-            if(i == 0|| dp[i-1]){
-                Trie node = root;
-                for(int j = i;j < s.length();j++){
-                    char ele = s.charAt(j);
-
-                    if(!node.child.containsKey(ele)){
+        boolean[] table = new boolean[s.length()]; // default value is false
+        
+        for(int i = 0; i<table.length; i++){
+            for(int j = 0; j<=i; j++){
+                if(j==0 || table[j-1]){
+                    if(wordDict.contains(s.substring(j, i+1)))
+                    {
+                        table[i] = true;
                         break;
                     }
-
-                    node = node.child.get(ele);
-                    if(node.isword){
-                        dp[j] = true;
-                    }
                 }
             }
         }
-
-        return dp[s.length()-1];
-
+        
+        return table[table.length - 1];
     }
 }
