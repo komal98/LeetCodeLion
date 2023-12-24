b7e3b920c390c9a89f6f822267ffb37b16d700d7
Komal Bakshi <kbakshi98@gmail.com>
2023-12-24 15:20:01 +0530

    Time: 0 ms (100.00%) | Memory: 57.5 MB (5.03%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,17 +1,25 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int[] ans = new int[nums.length];
-        for(int i= 0; i<nums.length; i++){
-            int pos = i+k;
-            if(pos<nums.length){
-                ans[pos] = nums[i];
-            }else{
-                pos = pos%nums.length;
-                ans[pos] = nums[i];
-            }
-        }
-        for(int i = 0; i<nums.length; i++){
-            nums[i] = ans[i];
+       int n = nums.length;
+       k = k%n;
+       reverse(nums,0,n-1);
+       reverse(nums,0,k-1);
+       reverse(nums,k,n-1);
+    }
+
+    public void reverse(int[] nums, int start, int end){
+        while(start<end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
 }
+
+/*
+[1,2,3,4,5,6,7] k = 3 the last three will go in the front
+[7,6,5,4,3,2,1] the first three rotate and remaining rotate
+[5,6,7,1,2,3,4]
+*/
