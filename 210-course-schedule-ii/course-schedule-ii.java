0363331f5842f989169ca93dba7ca253a03e90d6
Komal Bakshi <kbakshi98@gmail.com>
2024-07-26 20:53:46 -0700

    Time: 6 ms (55.35%) | Memory: 45.9 MB (44.59%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] pre) {
+        int[] indegree = new int[numCourses];
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for(int i =0; i<numCourses; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i =0; i<pre.length; i++){
+            adj.get(pre[i][1]).add(pre[i][0]);
+            indegree[pre[i][0]]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i =0; i<numCourses; i++){
+            if(indegree[i]==0){
+                queue.offer(i);
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        
+        int nodesVisited = 0;
+        while(!queue.isEmpty()){
+            int node = queue.poll();
+            nodesVisited++;
+            ans.add(node);
+
+            for(int neighbor : adj.get(node)){
+                indegree[neighbor]--;
+
+                if(indegree[neighbor] == 0){
+                    queue.offer(neighbor);
+                }
+            }
+        }
+
+        if(nodesVisited == numCourses){
+            return ans.stream().mapToInt(Integer::intValue).toArray();
+        }
+
+        return new int[0];
+
+    }
+}
