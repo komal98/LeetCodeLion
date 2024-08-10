f28ed3512a18db9342560788b5a8979b5b93a6ec
Komal Bakshi <kbakshi98@gmail.com>
2024-08-10 11:55:09 -0700

    Time: 7 ms (38.24%) | Memory: 45.8 MB (47.54%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,29 +1,29 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] pre) {
-        int[] indegree = new int[numCourses];
-        List<List<Integer>> adj = new ArrayList<>();
+       int[] indegree = new int[numCourses];
+       List<List<Integer>> adj = new ArrayList<>();
 
-        for(int i = 0; i<numCourses; i++){
-            adj.add(new ArrayList<>());
+       for(int i =0; i<numCourses; i++){
+        adj.add(new ArrayList<>());
+       }
+
+       for(int i = 0; i < pre.length; i++){
+        adj.get(pre[i][1]).add(pre[i][0]);
+        indegree[pre[i][0]]++;
+       }
+
+       Queue<Integer> queue = new LinkedList<>();
+
+       for(int i = 0; i < numCourses; i++){
+        if(indegree[i]== 0){
+            queue.offer(i);
         }
+       }
 
-        for(int i = 0; i<pre.length; i++){
-            adj.get(pre[i][1]).add(pre[i][0]);
-            indegree[pre[i][0]]++;
-        }
+       int nodesVisited = 0;
+       List<Integer> ans = new ArrayList<>();
 
-        Queue<Integer> queue = new LinkedList<>();
-
-        for(int i =0; i<numCourses; i++){
-            if(indegree[i] == 0){
-                queue.offer(i);
-            }
-        }
-
-        int nodesVisited = 0;
-        List<Integer> ans = new ArrayList<>();
-
-        while(!queue.isEmpty()){
+       while(!queue.isEmpty()){
             int node = queue.poll();
             nodesVisited++;
             ans.add(node);
@@ -34,14 +34,14 @@ class Solution {
                 if(indegree[neighbor] == 0){
                     queue.offer(neighbor);
                 }
-            }
-        }
 
-        if(nodesVisited == numCourses){
+            }
+       }
+
+       if(nodesVisited == numCourses){
              return ans.stream().mapToInt(Integer::intValue).toArray();
         }
 
         return new int[0];
-
     }
 }
