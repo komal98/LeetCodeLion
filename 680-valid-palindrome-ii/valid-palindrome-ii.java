e1f230d6eb5149dc06b5927fcfa69a8ff137c598
Komal Bakshi <kbakshi98@gmail.com>
2024-12-09 20:44:23 -0800

    Time: 4 ms (99.70%) | Memory: 46 MB (5.79%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,31 +1,28 @@
 class Solution {
-    private boolean checkPalindrome(String s, int i, int j) {
-        while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) {
-                return false;
-            }
-            
-            i++;
-            j--;
-        }
-        
-        return true;
-    }
-    
     public boolean validPalindrome(String s) {
         int i = 0;
         int j = s.length() - 1;
-        
-        while (i < j) {
-            // Found a mismatched pair - try both deletions
-            if (s.charAt(i) != s.charAt(j)) {
-                return (checkPalindrome(s, i, j - 1) || checkPalindrome(s, i + 1, j));
+
+        while( i < j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return (checkPalindrome(s,i+1,j) || checkPalindrome(s,i,j-1));
             }
-            
             i++;
             j--;
         }
-        
+        return true;
+    }
+
+    private boolean checkPalindrome(String s, int i, int j){
+
+        while(i < j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+
         return true;
     }
 }
