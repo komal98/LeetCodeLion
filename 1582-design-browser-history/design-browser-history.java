10f3235509f187c9637c92724385e67c726d3a7e
Komal Bakshi <kbakshi98@gmail.com>
2024-12-13 12:44:30 -0800

    Time: 53 ms (41.33%) | Memory: 50.4 MB (80.65%) - LeetSync

--- 1582-design-browser-history/design-browser-history.java
+++ 1582-design-browser-history/design-browser-history.java
@@ -1,36 +1,48 @@
+class DLLNode {
+    public String data;
+    public DLLNode prev, next;
+
+    DLLNode(String url) {
+        data = url;
+        prev = null;
+        next = null;
+    }
+};
+
 class BrowserHistory {
-    Stack<String> container = new Stack<>();
-    Stack<String> fwd = new Stack<>();
+    private DLLNode linkedListHead;
+    private DLLNode current;
+
     public BrowserHistory(String homepage) {
-        container.add(homepage);
+        // 'homepage' is the first visited URL.
+        linkedListHead = new DLLNode(homepage);
+        current = linkedListHead;
     }
     
     public void visit(String url) {
-        container.add(url);
-        fwd = new Stack<>();
+        // Insert new node 'url' in the right of current node.
+        DLLNode newNode = new DLLNode(url);
+        current.next = newNode;
+        newNode.prev = current;
+        // Make this new node as current node now.
+        current = newNode;
     }
     
     public String back(int steps) {
-        while(container.size() > 1 && steps > 0){
-            fwd.add(container.pop());
+        // Move 'current' pointer in left direction.
+        while (steps > 0 && current.prev != null) {
+            current = current.prev;
             steps--;
         }
-        return container.peek();
+        return current.data;
     }
     
     public String forward(int steps) {
-        while(fwd.size() > 0 && steps > 0){
-            container.add(fwd.pop());
+        // Move 'current' pointer in right direction.
+        while (steps > 0 && current.next != null) {
+            current = current.next;
             steps--;
         }
-        return container.peek();
+        return current.data;
     }
 }
-
-/**
- * Your BrowserHistory object will be instantiated and called as such:
- * BrowserHistory obj = new BrowserHistory(homepage);
- * obj.visit(url);
- * String param_2 = obj.back(steps);
- * String param_3 = obj.forward(steps);
- */
